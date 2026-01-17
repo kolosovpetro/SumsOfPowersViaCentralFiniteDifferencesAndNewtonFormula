50cc899d2325bff7d187e2937b4205d0efb0b6e5
kolosovpetro <kolosovp94@gmail.com>
2026-01-17 14:51:25 +0100

    work in progress

--- mathematica/SumsOfPowersViaCentralFiniteDifferencesAndNewtonFormula.m
+++ mathematica/SumsOfPowersViaCentralFiniteDifferencesAndNewtonFormula.m
@@ -1,22 +1,25 @@
 (* ::Package:: *)
 
-BeginPackage["SumsOfPowersViaCentralFiniteDifferencesAndNewtonFormula`"]
+BeginPackage["CentralDifferences`"]
 
 (*BEGIN: Definitions *)
-CentralFactorial::usage=""
-FallingFactorial::usage=""
+CentralDifferenceInZero::usage=""
 CentralDifference::usage=""
-NewtonSeriesForPowerInZero::usage=""
-NewtonSeriesForPower::usage=""
-MultifoldSumOfPowersRecurrence::usage=""
-MultifoldSumsOfPowers::usage=""
-MultifoldSumsOfPowersOddPowers::usage=""
-EvenPower::usage=""
-SumsOfEvenPower::usage=""
+T::usage=""
+OddPowerIdentity1::usage=""
+KnuthOddPowerIdentity::usage=""
+TestIdentity::usage=""
+CentralFactorial::usage=""
 CentralFactorialNumber::usage=""
-NewtonSeriesForPowerInCentralFactorials::usage=""
-OddPowersNewtonSeries::usage=""
+RiordanPowerIdentity::usage=""
+RiordanPowerIdentity1::usage=""
+RiordanPowerIdentity2::usage=""
+RiordanPowerIdentity3::usage=""
+FallingFactorial::usage=""
+NewtonsFormulaForCentralDifferences::usage=""
+NewtonsFormulaForCentralDifferencesShifted::usage=""
 (*END: Definitions *)
+
 (* =========================================================================DOCS END=================================================================== *)
 
 (*BEGIN: Define 0^x = 1 for all x *)
@@ -29,28 +32,28 @@ Protect[Power];
 (* =========================================================================FUNCTIONS BEGIN=========================================================== *)
 
 (*BEGIN: Definitions *)
-CentralFactorial[n_, k_]:= n * Product[n+ k/2 - j, {j, 1, k-1}];
-FallingFactorial[n_, k_] := Product[n-j, {j, 0, k-1}];
+CentralDifferenceInZero[n_, k_] := Sum[(-1)^(j) * Binomial[k, j] * (k/2 - j)^n, {j, 0, k}];
+T[n_, k_] :=  1 / (k!) *  CentralDifferenceInZero[n,k];
 CentralDifference[x_, n_, k_] := Sum[(-1)^j * Binomial[k, j] * (x + k/2 -j)^n, {j, 0, k}];
-CentralFactorialNumber[n_, k_]:= 1/k! * CentralDifference[0, n, k];
-NewtonSeriesForPowerInZero[n_, m_] := Sum[1/(k+1) * Binomial[n+(k+1)/2-1, k] * CentralDifference[0, m+1, k+1], {k, 0, m+1}];
-NewtonSeriesForPower[t_, n_, m_] := Sum[1/(k+1) * Binomial[n-t+(k+1)/2-1, k] * CentralDifference[t, m+1, k+1], {k, 0, m+1}];
-NewtonSeriesForPowerInCentralFactorials[a_, x_, n_] := Sum[CentralFactorial[x-a, k] * 1/k! * CentralDifference[a, n, k], {k, 0, n}];
-MultifoldSumOfPowersRecurrence[r_, n_, m_]:= 0;
-MultifoldSumOfPowersRecurrence[r_, n_, m_]:= n^m /; r==0;
-MultifoldSumOfPowersRecurrence[r_, n_, m_]:= Sum[MultifoldSumOfPowersRecurrence[r-1, k, m], {k, 1, n}] /; r>0;
-MultifoldSumsOfPowers[r_, n_, m_]:= Sum[1/(k+1) * Binomial[n+(k+1)/2 - 1 + r, k+r] * CentralDifference[0, m+1, k+1], {k, 0, m+1}];
-MultifoldSumsOfPowersOddPowers[r_, n_, m_]:= Sum[1/(k+1) * Binomial[n+(k+1)/2 - 1 + r, k+r] * CentralDifference[0, 2m+2, k+1], {k, 0, 2m}];
-EvenPower[n_, m_] := Sum[1/(2k+1) * Binomial[n+(2k+1)/2-1, 2k] * CentralDifference[0, 2m+1, 2k+1], {k, 0, m}];
-SumsOfEvenPower[r_, n_, m_] := Sum[1/(2k+1) * Binomial[n+(2k+1)/2-1+r, 2k+r] * CentralDifference[0, 2m+1, 2k+1], {k, 0, m}];
-OddPowersNewtonSeries[t_, n_, m_] := Sum[1/(2k) * Binomial[n+k-t-1, 2k-1] * CentralDifference[t, 2m, 2k], {k, 1, m}];
+OddPowerIdentity1[n_, m_] := Sum[Binomial[n+k-1, 2k-1] * 1/(2k) * CentralDifference[0, 2m, 2k], {k, 1, m}];
+KnuthOddPowerIdentity[n_, m_] := Sum[(2k-1)! * T[2m, 2k] * Binomial[n+k-1, 2k-1], {k, 1, m}];
+TestIdentity[n_, m_, t_] := Sum[Binomial[n+k-1, 2k-1] * 1/(2k) * CentralDifference[t, 2m, 2k], {k, 1, m}];
+
+CentralFactorial[x_, k_] := 1 /; k==0;
+CentralFactorial[x_, k_] := x * Product[(x + k/2 -j), {j, 1, k-1}] /; k>0;
+
+CentralFactorialNumber[n_, k_] := 1/k! * CentralDifferenceInZero[n, k];
+RiordanPowerIdentity[x_, m_] := Sum[T[m,k] * CentralFactorial[x, k], {k, 1, m}];
+RiordanPowerIdentity1[x_, m_] := Sum[1/k! * CentralDifference[0, m, k] * CentralFactorial[x, k], {k, 1, m}];
+RiordanPowerIdentity2[x_, m_, t_] := Sum[1/k! * CentralDifference[t, m, k] * CentralFactorial[x, k], {k, 1, m}];
+RiordanPowerIdentity3[n_, m_] := Sum[T[2m, 2k] * CentralFactorial[n, 2k], {k, 1, m}];
+FallingFactorial[x_, n_] := Product[x-k, {k, 0, n-1}];
+NewtonsFormulaForCentralDifferences[n_, m_] := Sum[CentralFactorial[n, j] * 1/j! * CentralDifference[0, m, j], {j, 0, m}];
+NewtonsFormulaForCentralDifferencesShifted[x_, h_, n_] := Sum[ 1/k! * CentralDifference[h, n, k] * CentralFactorial[x, k], {k, 0, n}];
 (*END: Definitions *)
 
 End[ ]
 EndPackage[ ]
-
-
-
 
 
 
